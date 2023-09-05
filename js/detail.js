c8dfec2c84b9419d9083eb42e56e5fdb145a3d5a
Lorena De Armas <lorenadearmas01@gmail.com>
2023-09-05 18:29:19 -0300

    styled-detail

--- js/detail.js
+++ js/detail.js
@@ -13,7 +13,7 @@ const createDetailTemplate = (item) => {
     let template = ''
     template = `
         <div class="card w-100">
-            <div class="row g-0">
+            <div class="row g-0 align-item-center justify-content-evenly align-content-around">
                 <div class="col-md-6">
                     <img
                         src=${item.image}
@@ -23,7 +23,7 @@ const createDetailTemplate = (item) => {
                 </div>
                 <div class="vr md-2"></div>
                 <div class="col-md-4">
-                    <div class="card-body">
+                    <div class="card-body dataBody">
                         <h4 class="card-title text-center">
                             ${item.name}
                         </h4>
